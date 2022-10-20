f13ac423b5905e627a61859aba204f3c2d6600a5
Alex <alexcruzsilva12@gmail.com>
2022-10-20 20:57:51 +0100

    FIXING URL 4

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,6 +6,10 @@ export const authOptions = {
     GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      }),
+      FacebookProvider({
+        clientId: process.env.FACEBOOK_CLIENT_ID,
+        clientSecret: process.env.FACEBOOK_CLIENT_SECRET
       })
   ],
   secret: process.env.JWT_SECRET ,
