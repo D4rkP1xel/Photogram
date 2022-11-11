e1b88b07d0dfa76289dda6c91ffc61c7e1cc471c
Alex <alexcruzsilva12@gmail.com>
2022-11-11 15:45:22 +0000

    fucking comment fix jesus christ cancer shit

--- pages/post/[post_id].js
+++ pages/post/[post_id].js
@@ -66,12 +66,22 @@ function PostPage() {
                 </div>
 
                 <div className='w-full md:max-w-[600px] px-6 mx-auto'>
-                    <div className='md:max-w-[600px] sm:mx-auto w-full '>
-                        <div className='flex gap-4 items-center py-4'>
+                    <div className='md:max-w-[600px] sm:mx-auto w-full'>
+                        <div className='flex gap-4 pt-4'>
                             {postInfo !== undefined ?
                                 <>
-                                    <img className="h-12 rounded-full cursor-pointer" draggable="false" src={postInfo.author_photo_url} alt="" referrerPolicy="no-referrer" />
-                                    <div className='font-medium select-none cursor-pointer'>{postInfo.author_username}</div>
+                                    
+                                        <div className='flex items-center gap-4 flex-shrink-0 h-fit'>
+                                           
+                                            <img className="h-12 rounded-full cursor-pointer" draggable="false" src={postInfo.author_photo_url} alt="" referrerPolicy="no-referrer" />
+                                            <div className=''>
+                                                <div className='font-medium select-none cursor-pointer'>{postInfo.author_username}</div>
+                                            </div>
+                                        </div>
+                                   
+                                    <div className='w-fit break-words mt-3'>Great photo!!!! I really liked the background and the colors and the sun and the bitches and cocaine.</div>
+
+
                                 </>
                                 :
                                 <>
@@ -80,44 +90,32 @@ function PostPage() {
                                 </>
                             }
                         </div>
-                        {postInfo !== undefined ?
-                            <div className='select-none text-gray-400 absolute text-[10px] tracking-wide'>
-                                <div className='relative bottom-6 left-16'>Posted {toDate(postInfo.date)}</div>
-                            </div>
-                            : ""}
+
+
                     </div>
 
-                    <hr className='md:max-w-[600px] sm:mx-auto w-full ' />
-
                     {postInfo !== undefined ?
-                        postInfo.description !== null ?
-                            <div className='mb-4 md:max-w-[600px] sm:mx-auto w-full '>
-                                <div className='flex gap-4 items-center pt-4 pb-2'>
-                                    <img className="h-12 rounded-full cursor-pointer" draggable="false" src={postInfo.author_photo_url} alt="" referrerPolicy="no-referrer" />
-                                    <div className='font-medium select-none cursor-pointer'>{postInfo.author_username}</div>
-                                </div>
-                                <div className='select-none text-gray-400 absolute text-[10px] tracking-wide'>
-                                    <div className='relative bottom-5 left-16'><span>{toDate(postInfo.date)}</span></div>
-                                </div>
-                                <div className='lg:pl-4 lg:pr-0 sm:px-16 px-4'>{postInfo.description}</div>
-                            </div>
-                            : ""
-                        : ""
+                        <hr className='md:max-w-[600px] sm:mx-auto w-full my-2' />
+                        :
+                        ""
                     }
-                    
+
+
+
+
                     {comments !== undefined ?
                         comments?.map((comment, index) => {
                             return (
-                            <div key={index} className='mb-4 md:max-w-[600px] sm:mx-auto w-full '>
-                                <div className='flex gap-4 items-center py-4'>
-                                    <img className="h-12 rounded-full cursor-pointer" draggable="false" src={comment.user_photo_url} alt="image" referrerPolicy="no-referrer" />
-                                    <div className='font-medium select-none cursor-pointer'>{comment.user_username}</div>
+                                <div key={index} className='mb-4 md:max-w-[600px] sm:mx-auto w-full '>
+                                    <div className='flex gap-4 items-center py-4'>
+                                        <img className="h-12 rounded-full cursor-pointer" draggable="false" src={comment.user_photo_url} alt="image" referrerPolicy="no-referrer" />
+                                        <div className='font-medium select-none cursor-pointer'>{comment.user_username}</div>
+                                    </div>
+                                    <div className='select-none text-gray-400 absolute text-[10px] tracking-wide'>
+                                        <div className='relative bottom-6 left-16'><div className='flex gap-2'><span>{toDate(comment.date)}</span><span className='font-semibold text-gray-500 cursor-pointer'>reply</span></div></div>
+                                    </div>
+                                    <div className='lg:pl-4 lg:pr-0 sm:px-16 px-4'>{comment.text}</div>
                                 </div>
-                                <div className='select-none text-gray-400 absolute text-[10px] tracking-wide'>
-                                    <div className='relative bottom-6 left-16'><div className='flex gap-2'><span>{toDate(comment.date)}</span><span className='font-semibold text-gray-500 cursor-pointer'>reply</span></div></div>
-                                </div>
-                                <div className='lg:pl-4 lg:pr-0 sm:px-16 px-4'>{comment.text}</div>
-                            </div>
                             )
                         })
                         :
