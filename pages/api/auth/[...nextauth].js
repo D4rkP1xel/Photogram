dfa786b23123768431b4a6d8f5c351c0cbb389f8
Alex <alexcruzsilva12@gmail.com>
2022-11-29 11:46:33 +0000

    idk

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -15,6 +15,16 @@ export const authOptions = {
   secret: process.env.JWT_SECRET ,
   pages: {
     signIn: '/signin'
+  },
+  callbacks: {
+    async jwt({ token, account, profile }) {
+      // Persist the OAuth access_token and or the user id to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token
+        token.id = profile.id
+      }
+      return token
+    }
   }
 }
 export default NextAuth(authOptions)
