ca0a976b453dd5ab1a6dadb6e491016f80a53bcb
Alex <alexcruzsilva12@gmail.com>
2022-11-29 12:09:02 +0000

    idk8

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -20,17 +20,14 @@ export const authOptions = {
     async jwt({ token, account, profile }) {
       // Persist the OAuth access_token and or the user id to the token right after signin
       if (profile) {
-        token.accessToken = account.access_token
-        token.id = profile.id
         token.provider = account.provider
       }
       return token
     },
     async session({ session, token, user }) {
       // Send properties to the client, like an access_token and user id from a provider.
-      session.accessToken = token.accessToken
-      session.user.id = token.id
       session.user.provider = token.provider || "idk"
+      session.token = token
       return session
     }
   }
